75a8648f03f8b1db488e03abd79aeb692ab5b8f5
anhlh74 <anhlhfw@gmail.com>
2025-02-11 17:41:36 +0700

    acacaca

--- server.js
+++ server.js
@@ -56,8 +56,8 @@ app.use(express.static(path.join(__dirname, "public")));
 require("./routes")(app);
 
 app.on("ready", () => {
-  app.listen(3000, () => {
-    console.log("Server is up on port", 3000);
+  app.listen(8080, () => {
+    console.log("Server is up on port", 8080);
   });
 });
 
