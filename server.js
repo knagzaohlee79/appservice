a3a229fa1c3e9913ce58b810c7ddfc7ffc032103
anhlh74 <anhlhfw@gmail.com>
2025-02-11 17:22:45 +0700

    up

--- server.js
+++ server.js
@@ -1,6 +1,37 @@
-/**
- * Created by Syed Afzal
- */
+// /**
+//  * Created by Syed Afzal
+//  */
+// require("./config/config");
+
+// const express = require("express");
+// const path = require("path");
+// const cookieParser = require("cookie-parser");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const db = require("./db");
+
+// const app = express();
+
+// //connection from db here
+// db.connect(app);
+
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, "public")));
+
+// //  adding routes
+// require("./routes")(app);
+
+// app.on("ready", () => {
+//   app.listen(3000, () => {
+//     console.log("Server is up on port", 3000);
+//   });
+// });
+
+// module.exports = app;
+
 require("./config/config");
 
 const express = require("express");
@@ -12,7 +43,7 @@ const db = require("./db");
 
 const app = express();
 
-//connection from db here
+// Không cần kết nối MongoDB nữa
 db.connect(app);
 
 app.use(cors());
@@ -21,7 +52,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-//  adding routes
+//  Adding routes
 require("./routes")(app);
 
 app.on("ready", () => {
